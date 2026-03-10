b2113e88ac56bac5df10780ef8064b927038058c
chandraketukumar3 <s8ulwizard1319@gmail.com>
2026-03-10 12:54:17 +0530

    fix tmdb results structure

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios"
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api"
-    : "https://noxmovies.onrender.com/api"
+    : "https://nox-movies-weld.vercel.app/api"
 
 // Axios instance
 const api = axios.create({
